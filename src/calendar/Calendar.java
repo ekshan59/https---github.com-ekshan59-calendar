e6c67c481a0f750b4ddf27fb10df4e5df294a69c
ekshan59 <ekshan59@abv.bg>
2022-06-16 10:46:42 +0300

    Това е първо качване

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -1,7 +1,12 @@
 package calendar;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.util.Scanner;
-
 
 /*
  * 5. Личен календар
@@ -74,7 +79,7 @@ import java.util.Scanner;
 
 public class Calendar {
 	// избор на опция
-	public static void chooseEvent(int a) {
+	public static void chooseEvent(int a) throws IOException {
 
 		switch (a) {
 		case 1:
@@ -86,14 +91,63 @@ public class Calendar {
 		case 3:
 			arhiveEvent();
 			break;
-
 		}
-
+	} 
+	
+	public static void enterName() throws FileNotFoundException {
+		File file = new File("arhive.txt");
+		PrintWriter pw = new PrintWriter(file);
+		pw.println("Nomer");
+		pw.close();
+	}
+	
+	public static void enterDate() {
+		
+	}
+	
+	public static void enterStartTime() {
+		
+	}
+	
+	public static void enterEndTine() {
+		
+	}
+	
+	public static void enterDescription() {
+		
 	}
 
 	// създаване на събитие
 	public static void createEvent() {
+		BufferedReader br = null;
+		try {
 
+			File file = new File("arhive.txt");
+
+			if (!file.exists())
+				file.createNewFile();
+
+			PrintWriter pw = new PrintWriter(file);
+			pw.println("Хотел");
+			pw.println("Резервация");
+			pw.println("Три дена");
+			pw.close();
+
+			br = new BufferedReader(new FileReader("arhive.txt"));
+			String line;
+			while ((line = br.readLine()) != null) {
+				System.out.println(line);
+			}
+		} catch (IOException e) {
+			System.out.println("error: " + e);
+		} finally {
+			try {
+				br.close();
+			} catch (IOException e) {
+				System.out.println("error: " + e);
+			}
+			
+		}
 	}
 
 	// премахване на събитие
@@ -102,29 +156,33 @@ public class Calendar {
 	}
 
 	// създадени събития
-	public static void arhiveEvent() {
-		//File file = new File("arhive");
-		// Scanner sc = new Scanner(file);
-		// System.out.println(file);
-	}
-
-	public static void main(String[] args) {
-
-		System.out.println("---МОЯТ КАЛЕНДАР---");
-		Scanner sc = new Scanner(System.in);
-		System.out.println("--Изберете опция:--\nЗа създаване на събитие:  1  \n"
-				+ "За премахване на събитие: 2  \nВашите създадени събития: 3");
-
-		int event = sc.nextInt();
-		if (event == 1 || event == 2 || event == 3) {
-			chooseEvent(event);
-		} else {
-			System.out.println("Такава опция не съществува?\nОпитай пак:");
-			System.out.println();
-			main(args);
+	public static void arhiveEvent() throws IOException {
+		BufferedReader br = null;
+		br = new BufferedReader(new FileReader("arhive.txt"));
+		String line;
+		while ((line = br.readLine()) != null) {
+			System.out.println(line);
 		}
-		sc.close();
-
+		br.close();
+		
 	}
 
+	public static void main(String[] args) throws IOException {
+		
+			
+			  System.out.println("---МОЯТ КАЛЕНДАР---"); 
+			  Scanner sc = new Scanner(System.in);
+			  System.out.println("--Изберете опция:--\nЗа създаване на събитие:  1  \n" +
+			  "За премахване на събитие: 2  \nВашите създадени събития: 3");
+			  
+			  int event = sc.nextInt(); 
+			  if (event == 1 || event == 2 || event == 3) {
+			  chooseEvent(event); 
+			  } else {
+			  System.out.println("Такава опция не съществува?\nОпитай пак:");
+			  System.out.println(); 
+			  main(args); 
+			  } sc.close();
+			
+	}
 }
