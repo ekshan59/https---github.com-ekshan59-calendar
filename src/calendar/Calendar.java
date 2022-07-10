257fbcb76fda3c402518bfe7bb24025e2875a4fd
ekshan59 <ekshan59@abv.bg>
2022-07-10 17:19:18 +0300

    Нов вриант

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -1,170 +1,323 @@
 package calendar;
-import java.sql.SQLException;
-import java.util.ArrayList;
+
 import java.util.Scanner;
 
 public class Calendar {
 
-	//Избор за създаване на събитие
-	public static void optionEvent() {
+	// Промяна на името
+	public static void changesName(List l) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("\nИзбери начин на създаване:\n1 Създай със String\n2 Създай с ArrayList");
+		System.out.println("Въведете име: ");
+		l.name = sc.nextLine();
 
-		int a = sc.nextInt();
-		if (a == 1) {
-			createString();
-		} else if (a == 2) {
-			createArray();
+		System.out.println("\nИме " + l.name + "\n" + "Дата " + l.day + "." + l.month + "." + l.year + "\n"
+				+ "Начало от " + l.start + "." + l.mm + "\n" + "Край до " + l.end + "." + l.mmn + "\n" + "Описание "
+				+ l.descript);
+	}
+
+	// Промяна на датата
+	public static void changesDate(List l) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Въведете ден: ");
+		l.day = sc.nextInt();
+		System.out.println("Въведете месец: ");
+		l.month = sc.nextInt();
+		System.out.println("Въведете година: ");
+		l.year = sc.nextInt();
+
+		// дали годината е високосна
+		boolean leapYеar = (l.year % 4 == 0 && (l.year % 400 == 0 || l.year % 100 != 0));
+		// валидация на данните
+		boolean correctData = true;
+		if (l.day < 0 || l.day > 31) {
+			correctData = false;
+		} else if (l.month < 0 || l.month > 12) {
+			correctData = false;
+		} else if (l.year < 2022 || l.year > 2023) {// Граница на годината
+			correctData = false;
+		} else if (l.day == 29 && l.month == 2 && !leapYеar) {
+			correctData = false;
+		}
+		int daysInMonth = 31;
+		int numOfMonths = 12;
+		// ако погледнем броя дни в месеците, виждаме чете са 31 като в четните месеци
+		// до 7-мия включително имаме 30дни, а в нечетните от 7-мия нататък пак имаме 30
+		// дни
+		if ((l.month % 2 == 0 && l.month <= 7) || (l.month % 2 == 1 && l.month > 7)) {
+			daysInMonth = 30;
+		}
+		// а във февруари имаме 28 или 29 дни
+		if (l.month == 2) {
+			daysInMonth = 28;
+			if (leapYеar) {
+				daysInMonth++;
+			}
+		}
+		l.day++;
+		// увеличаваме месеца като ако дните са минали броя дни в месеца ще получим 1
+		// примерно : 32 ден / 31 дена в месеца = 1 и това ще добавим към месеца
+		l.month += l.day / daysInMonth;
+		// ако сме преминали броя дни в месеца с остатък при деление на броя дни ще
+		// получим 1-ви ден от новия месец в противен случай деня ще си остане същия
+		// примерно 32 ден % 31= 1, т.е. първи ден от новия месец
+		l.day %= daysInMonth;
+		// по същия начин процедираме и с годината и месеца
+		l.year += l.month / numOfMonths;
+		l.month %= numOfMonths;
+		if (correctData) {
+			System.out.println("Следващата дата е");
+			System.out.println("\nИме: " + l.name + "\nДата: " + (l.day - 1) + "." + l.month + "." + l.year
+					+ "\nНачало от: " + l.start + "." + l.mm + "\nКрай до: " + l.end + "." + l.mmn + "\n" + "Описание: "
+					+ l.descript + "\n");
+		} else if (correctData == false) {
+			// ако в кода някъде имаме невалидни данни за въведена дата, то тогава
+			// променливата correctData е false.
+			System.out.println("\nНевалидна дата");
+			System.out.println("Въведи отново\n");
+
+			changesDate(l);
+
 		} else {
-			System.err.println("\nТакава опция не съществува?\nОпитай пак:\n");
+			// дали годината е високосна
+			leapYеar = (l.year % 4 == 0 && (l.year % 400 == 0 || l.year % 100 != 0));
+			// валидация на данните
+			correctData = true;
+			if (l.day < 0 || l.day > 31) {
+				correctData = false;
+			} else if (l.month < 0 || l.month > 12) {
+				correctData = false;
+			} else if (l.year < 2022 || l.year > 2023) {// Граница на годината
+				correctData = false;
+			} else if (l.day == 29 && l.month == 2 && !leapYеar) {
+				correctData = false;
+			}
+			daysInMonth = 31;
+			numOfMonths = 12;
+			// ако погледнем броя дни в месеците, виждаме чете са 31 като в четните месеци
+			// до 7-мия включително имаме 30дни, а в нечетните от 7-мия нататък пак имаме 30
+			// дни
+			if ((l.month % 2 == 0 && l.month <= 7) || (l.month % 2 == 1 && l.month > 7)) {
+				daysInMonth = 30;
+			}
+			// а във февруари имаме 28 или 29 дни
+			if (l.month == 2) {
+				daysInMonth = 28;
+				if (leapYеar) {
+					daysInMonth++;
+				}
+			}
+			l.day++;
+			// увеличаваме месеца като ако дните са минали броя дни в месеца ще получим 1
+			// примерно : 32 ден / 31 дена в месеца = 1 и това ще добавим към месеца
+			l.month += l.day / daysInMonth;
+			// ако сме преминали броя дни в месеца с остатък при деление на броя дни ще
+			// получим 1-ви ден от новия месец в противен случай деня ще си остане същия
+			// примерно 32 ден % 31= 1, т.е. първи ден от новия месец
+			l.day %= daysInMonth;
+			// по същия начин процедираме и с годината и месеца
+			l.year += l.month / numOfMonths;
+			l.month %= numOfMonths;
+			if (correctData) {
+				System.out.println("Следващата дата е");
+
+				System.out.println("\nИме: " + l.name + "\nДата: " + (l.day - 1) + "." + l.month + "." + l.year
+						+ "\nНачало от: " + l.start + "." + l.mm + "\nКрай до: " + l.end + "." + l.mmn + "\n"
+						+ "Описание: " + l.descript + "\n");
+
+			} else {
+				// ако в кода някъде имаме невалидни данни за въведена дата, то тогава
+				// променливата correctData е false.
+				System.out.println("Невалидна дата");
+				System.out.println("Въведи отново\n");
+				int num = 2;
+				changesDate(l);
+
+			}
 		}
 	}
 
-	//Търсене на събитие по дата
-	public static void eventOption() {
+	// Промяна на начален час
+	public static void changesStart(List l) {
 		Scanner sc = new Scanner(System.in);
-		System.out
-				.println("Намери събитие по дата: \n20 " + "За събитие на 20.08.2022г.\n26 За събитие на 26.08.2022г.");
-		int b = sc.nextInt();
-		switch (b) {
-		case 20:
-			readyEvent();
-			break;
-		case 26:
-			newEvent();
-			break;
-		default:
-			System.err.println("\nТакава опция не съществува?\nОпитай пак:\n");
-		}
-	}
-
-	//Избрана опция за изпълнение
-	public static void chooseEvent(int a) {
-		if (a == 1) {
-			System.out.println("Моля създайте събитие: ");
-			optionEvent();
-		} else if (a == 2) {
-			listEvent();
-		} else if (a == 3) {
-			eventOption();
-		} else if (a == 4) {
-			availableEvent();
-		}
-	}
-
-	//Налични опции
-	public static void availableEvent() {
-		readyEvent();
-		newEvent();
-	}
-
-	//Създаване чрез String
-	public static void createString() {
-
-		Scanner sc = new Scanner(System.in);
-
-		System.out.println("Въведете име: ");
-		String name = (sc.nextLine());
-
-		System.out.println("Въведете дата: ");
-		String date = (sc.nextLine());
-
 		System.out.println("Въведете начален час: ");
-		String start = (sc.nextLine());
+		l.start = sc.nextInt();
+		System.out.println("Въведете минути: ");
+		l.mm = sc.nextInt();
 
-		System.out.println("Въведете краен час: ");
-		String end = (sc.nextLine());
-
-		System.out.println("Въведете описание: ");
-		String descript = (sc.nextLine());
-
-		String result = ("\nИме: " + name + "\nДата: " + date + "\nНачало от " 
-				+ start + "\nКрай до " + end + "\n" + "Описание: " + descript);
-		System.out.println(result);
-		System.out.println("Успешно създадено стбитие!\n");
+		System.out.println("\nИме " + l.name + "\n" + "Дата " + l.day + "." + l.month + "." + l.year + "\n"
+				+ "Начало от " + l.start + "." + l.mm + "\n" + "Край до " + l.end + "." + l.mmn + "\n" + "Описание "
+				+ l.descript);
 	}
 
-	//Създадено събитие 
-	public static void readyEvent() {
-		ArrayList<String> event = new ArrayList<String>();
-		event.add("\nИме: Банка 'ДСК'");
-		event.add("Дата: 20.08.2022г.");
-		event.add("Начало от: 09.00ч.");
-		event.add("Край: до 10.00ч.");
-		event.add("Информация за кредит\n");
-		for (String i : event) {
-			System.out.println(i);
-		}
-	}
-
-	//Създадено събитие
-	public static void newEvent() {
-		String name = "Дигитално общество";
-		String date = "26.08.2022г";
-		String start = "13.00ч.";
-		String end = "14.00ч.";
-		String descript = "Записване за курс";
-		String result = ("\nИме: " + name + "\nДата: " + date + "\nНачало От: " + 
-				start + "\nКрай до: " + end + "\n" + "Описание: " + descript + "\n");
-		System.out.println(result);
-	}
-
-	//Създадена програма за деня
-	public static void listEvent() {
-		System.out.println("\n===Програма за деня:===");
-		ArrayList<String> event = new ArrayList<String>();
-		event.add("От 08.00ч. до 09.00ч. 'Среща с екипа'");
-		event.add("От 11.00ч. до 12.00ч. 'Прием на клиент'");
-		event.add("От 14.00ч. до 15.00ч. 'Преинсталация на компютър'");
-		event.add("От 16.00ч. до 17.00ч. 'Обновяване на сайта'");
-		event.add("От 19/00ч. до 21.00ч. 'Вечеря на открито'\n");
-		for (String i : event) {
-			System.out.println(i);
-		}
-
-	}
-
-	//Създаване чрез ArrayList
-	public static void createArray() {
-		
+	// Промяна на краен час
+	public static void changesEnd(List l) {
 		Scanner sc = new Scanner(System.in);
-
-		ArrayList<String> event = new ArrayList<String>();
-		System.out.println("Въведете име: ");
-		event.add(sc.nextLine());
-		System.out.println("Въведете дата: ");
-		event.add(sc.nextLine());
-		System.out.println("Въведете начален час: ");
-		event.add(sc.nextLine());
 		System.out.println("Въведете краен час: ");
-		event.add(sc.nextLine());
-		System.out.println("Въведете описание: ");
-		event.add(sc.nextLine());
-		System.out.println();
-		
-		for (String i : event) {
-			System.out.println(i);
-		}
-		System.out.println("Успешно създадено стбитие!\n");
+		l.end = sc.nextInt();
+		System.out.println("Въведете минути: ");
+		l.mmn = sc.nextInt();
+
+		System.out.println("\nИме " + l.name + "\n" + "Дата " + l.day + "." + l.month + "." + l.year + "\n"
+				+ "Начало от " + l.start + "." + l.mm + "\n" + "Край до " + l.end + "." + l.mmn + "\n" + "Описание "
+				+ l.descript);
 	}
 
-	//Извеждане в конзолата
-	public static void main(String[] args) throws SQLException {
+	// Прпомяна на описанието
+	public static void changesDesctipt(List l) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Въведете описание: ");
+		l.descript = sc.nextLine();
+
+		System.out.println("\nИме " + l.name + "\n" + "Дата " + l.day + "." + l.month + "." + l.year + "\n"
+				+ "Начало от " + l.start + "." + l.mm + "\n" + "Край до " + l.end + "." + l.mmn + "\n" + "Описание "
+				+ l.descript);
+	}
+
+	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
 		while (true) {
-			System.out.println("  ---МОЯТ КАЛЕНДАР---");
-			System.out.println("  --Изберете опция:--\n1 За създаване на събитие:\n"
-					+ "2 Дневен график:\n3 Търсене на събитие:\n4 Намерете наличност:\n5 Изход");
+			List l = new List();
+			System.out.println("Въведете име: ");
+			l.name = sc.nextLine();
+			System.out.println("Въведете ден: ");
+			l.day = sc.nextInt();
+			System.out.println("Въведете месец: ");
+			l.month = sc.nextInt();
+			System.out.println("Въведете година: ");
+			l.year = sc.nextInt();
 
-			int command = sc.nextInt();
+			// дали годината е високосна
+			boolean leapYеar = (l.year % 4 == 0 && (l.year % 400 == 0 || l.year % 100 != 0));
+			// валидация на данните
+			boolean correctData = true;
+			if (l.day < 0 || l.day > 31) {
+				correctData = false;
+			} else if (l.month < 0 || l.month > 12) {
+				correctData = false;
+			} else if (l.year < 2022 || l.year > 2023) {// Граница на годината
+				correctData = false;
+			} else if (l.day == 29 && l.month == 2 && !leapYеar) {
+				correctData = false;
+			}
+			int daysInMonth = 31;
+			int numOfMonths = 12;
+			// ако погледнем броя дни в месеците, виждаме чете са 31 като в четните месеци
+			// до 7-мия включително имаме 30дни, а в нечетните от 7-мия нататък пак имаме 30
+			// дни
+			if ((l.month % 2 == 0 && l.month <= 7) || (l.month % 2 == 1 && l.month > 7)) {
+				daysInMonth = 30;
+			}
+			// а във февруари имаме 28 или 29 дни
+			if (l.month == 2) {
+				daysInMonth = 28;
+				if (leapYеar) {
+					daysInMonth++;
+				}
+			}
+			l.day++;
+			// увеличаваме месеца като ако дните са минали броя дни в месеца ще получим 1
+			// примерно : 32 ден / 31 дена в месеца = 1 и това ще добавим към месеца
+			l.month += l.day / daysInMonth;
+			// ако сме преминали броя дни в месеца с остатък при деление на броя дни ще
+			// получим 1-ви ден от новия месец в противен случай деня ще си остане същия
+			// примерно 32 ден % 31= 1, т.е. първи ден от новия месец
+			l.day %= daysInMonth;
+			// по същия начин процедираме и с годината и месеца
+			l.year += l.month / numOfMonths;
+			l.month %= numOfMonths;
+			if (correctData) {
+				System.out.println("Следващата дата е");
+				System.out.println("\nИме: " + l.name + "\nДата: " + (l.day - 1) + "." + l.month + "." + l.year
+						+ "\nНачало от: " + l.start + "." + l.mm + "\nКрай до: " + l.end + "." + l.mmn + "\n"
+						+ "Описание: " + l.descript + "\n");
+			} else if (correctData == false) {
+				// ако в кода някъде имаме невалидни данни за въведена дата, то тогава
+				// променливата correctData е false.
+				System.out.println("\nНевалидна дата");
+				System.out.println("Въведи отново\n");
 
-			if (command == 1 || command == 2 || command == 3 || command == 4) {
-				chooseEvent(command);
-			} else if (command == 5) {
-				System.exit(0);
+				System.out.println("Въведете ден: ");
+				l.day = sc.nextInt();
+
 			} else {
-				System.err.println("\nТакава опция не съществува?\nОпитай пак:\n");
+				// дали годината е високосна
+				leapYеar = (l.year % 4 == 0 && (l.year % 400 == 0 || l.year % 100 != 0));
+				// валидация на данните
+				correctData = true;
+				if (l.day < 0 || l.day > 31) {
+					correctData = false;
+				} else if (l.month < 0 || l.month > 12) {
+					correctData = false;
+				} else if (l.year < 2022 || l.year > 2023) {// Граница на годината
+					correctData = false;
+				} else if (l.day == 29 && l.month == 2 && !leapYеar) {
+					correctData = false;
+				}
+				daysInMonth = 31;
+				numOfMonths = 12;
+				// ако погледнем броя дни в месеците, виждаме чете са 31 като в четните месеци
+				// до 7-мия включително имаме 30дни, а в нечетните от 7-мия нататък пак имаме 30
+				// дни
+				if ((l.month % 2 == 0 && l.month <= 7) || (l.month % 2 == 1 && l.month > 7)) {
+					daysInMonth = 30;
+				}
+				// а във февруари имаме 28 или 29 дни
+				if (l.month == 2) {
+					daysInMonth = 28;
+					if (leapYеar) {
+						daysInMonth++;
+					}
+				}
+				l.day++;
+				// увеличаваме месеца като ако дните са минали броя дни в месеца ще получим 1
+				// примерно : 32 ден / 31 дена в месеца = 1 и това ще добавим към месеца
+				l.month += l.day / daysInMonth;
+				// ако сме преминали броя дни в месеца с остатък при деление на броя дни ще
+				// получим 1-ви ден от новия месец в противен случай деня ще си остане същия
+				// примерно 32 ден % 31= 1, т.е. първи ден от новия месец
+				l.day %= daysInMonth;
+				// по същия начин процедираме и с годината и месеца
+				l.year += l.month / numOfMonths;
+				l.month %= numOfMonths;
+				if (correctData) {
+					System.out.println("Следващата дата е");
+
+					System.out.println("\nИме: " + l.name + "\nДата: " + (l.day - 1) + "." + l.month + "." + l.year
+							+ "\nНачало от: " + l.start + "." + l.mm + "\nКрай до: " + l.end + "." + l.mmn + "\n"
+							+ "Описание: " + l.descript + "\n");
+
+				} else if (correctData == false) {
+					// ако в кода някъде имаме невалидни данни за въведена дата, то тогава
+					// променливата correctData е false.
+					System.out.println("Невалидна дата");
+					System.out.println("Въведи отново\n");
+					// int num = 2;
+					// changesDate(l);
+					System.out.println("Въведете ден: ");
+					l.day = sc.nextInt();
+
+				} else {
+
+					System.out.println("Въведете начален час: ");
+					l.start = sc.nextInt();
+					System.out.println("Въведете минути: ");
+					l.mm = sc.nextInt();
+					System.out.println("Въведете краен час: ");
+					l.end = sc.nextInt();
+					System.out.println("Въведете минути: ");
+					l.mmn = sc.nextInt();
+					System.out.println("Въведете описание: ");
+					l.descript = sc.nextLine();
+					l.descript = sc.nextLine();
+
+					System.out.println("\nИме " + l.name + "\n" + "Дата " + l.day + "." + l.month + "." + l.year + "\n"
+							+ "Начало от " + l.start + "." + l.mm + "\n" + "Край до " + l.end + "." + l.mmn + "\n"
+							+ "Описание " + l.descript);
+
+					// main(args);
+					// changesName(l);
+				}
 			}
 		}
 	}
